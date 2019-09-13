5aaeddd74c32ebf244322c0caec2811df63b4e73
anton <menshickov@softmotions.com>
2019-09-13 19:05:07 +0700

    copy/paste passphrase

--- app/components/RestoreAccount/RecoveryPhraze/RecoveryPhrase.js
+++ app/components/RestoreAccount/RecoveryPhraze/RecoveryPhrase.js
@@ -1,5 +1,7 @@
 // @flow
+import { clipboard } from 'electron';
 import React, { Component } from 'react';
+import Icon from '../../common/Icon/Icon';
 import styles from './RecoveryPhraze.css';
 
 type Props = {
@@ -8,6 +10,8 @@ type Props = {
   readOnly?: boolean,
   onChange?: Function
 };
+
+const passPhaseSearchRegex = '(\\d+)\\. ([a-z].*)\\n';
 
 export default class RecoveryPhrase extends Component<Props> {
   props: Props;
@@ -36,14 +40,16 @@ export default class RecoveryPhrase extends Component<Props> {
     this.setState({
       values
     });
-    onChange(values);
+    if (typeof onChange === 'function') {
+      onChange(values);
+    }
   }
 
   renderPhrazeWords() {
     const { wordsCount, phrase, readOnly } = this.props;
-    const phrazeInputs = [];
+    const phraseInputs = [];
     for (let i = 0; i < wordsCount; i += 1) {
-      phrazeInputs.push(
+      phraseInputs.push(
         <div className={styles.InputContainer} key={i}>
           <span>{i + 1}.</span>
           <input
@@ -51,19 +57,108 @@ export default class RecoveryPhrase extends Component<Props> {
             readOnly={readOnly}
             name={phrase[i].id}
             onChange={e => this.handleInputChange(e)}
+            onPaste={e => e.preventDefault() || this.paste()}
             autoFocus={i === 0}
             className={styles.Input}
           />
         </div>
       );
     }
-    return phrazeInputs;
+    return phraseInputs;
+  }
+
+  copy() {
+    const { phrase } = this.props;
+    let result = '';
+    phrase.forEach((part, index) => {
+      result += `${index + 1}. ${part.value}\n`;
+    });
+    clipboard.writeText(result);
+  }
+
+  paste() {
+    const { wordsCount, onChange } = this.props;
+    const clipboardContent = clipboard.readText();
+    const regex = RegExp(passPhaseSearchRegex, 'g');
+    const values = [];
+    let word = regex.exec(clipboardContent);
+    if (word === null || word.length !== 3 || parseInt(word[1], 10) !== 1) {
+      return;
+    }
+    do {
+      values.push({ id: values.length, value: word[2] });
+      word = regex.exec(clipboardContent);
+    } while (
+      word !== null &&
+      values.length < 24 &&
+      values.length + 1 === parseInt(word[1], 10) &&
+      word.length === 3
+    );
+    if (values.length !== wordsCount) {
+      return;
+    }
+    this.setState({ values });
+    if (typeof onChange === 'function') {
+      onChange(values);
+    }
+  }
+
+  clear() {
+    const { onChange } = this.props;
+    const { values } = this.state;
+    const clearValues = values.map(v => ({ ...v, value: '' }));
+    this.setState({ values: clearValues });
+    if (typeof onChange === 'function') {
+      onChange(clearValues);
+    }
   }
 
   render() {
+    const { phrase, readOnly } = this.props;
+    const notEmpty = !!phrase.map(part => part.value).join('');
     return (
-      <div className={`${styles.Container} ScrollBar`}>
-        {this.renderPhrazeWords()}
+      <div className={styles.Wrapper}>
+        <div className={styles.ActionsPanel}>
+          {notEmpty && !readOnly && (
+            <div
+              onClick={() => this.clear()}
+              onKeyPress={() => false}
+              role="button"
+              tabIndex="-1"
+              className={styles.Action}
+              title="Clear all words"
+            >
+              <Icon name="clear" color="#fff" size={31} />
+            </div>
+          )}
+          {notEmpty && (
+            <div
+              onClick={() => this.copy()}
+              onKeyPress={() => false}
+              role="button"
+              tabIndex="-1"
+              className={styles.Action}
+              title="Copy"
+            >
+              <Icon name="content_copy" color="#fff" size={31} />
+            </div>
+          )}
+          {!readOnly && (
+            <div
+              onClick={() => this.paste()}
+              onKeyPress={() => false}
+              role="button"
+              tabIndex="-1"
+              className={styles.Action}
+              title="Paste"
+            >
+              <Icon name="content_paste" color="#fff" size={31} />
+            </div>
+          )}
+        </div>
+        <div className={`${styles.Container} ScrollBar`}>
+          {this.renderPhrazeWords()}
+        </div>
       </div>
     );
   }
